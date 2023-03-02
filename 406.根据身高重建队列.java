3cb80623d47d925c77e200f221db7e10e2287e05
WinsonHuang <hwangwc@qq.com>
2023-03-02 22:48:42 +0800

    406

---
+++ 406.根据身高重建队列.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=406 lang=java
+ *
+ * [406] 根据身高重建队列
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0];
+            }
+        });
+        List<int[]> queue = new ArrayList<>();
+        for (int[] ip : people) {
+            queue.add(ip[1], ip);
+        }
+        return queue.toArray(new int[queue.size()][]);
+    }
+}
+// @lc code=end
+
