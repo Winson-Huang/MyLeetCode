c11ffc7d37afb7df85c11c492f41581901df9e13
WinsonHuang <hwangwc@qq.com>
2023-03-06 20:37:19 +0800

    贪心53

---
+++ 53.最大子数组和.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=java
+ *
+ * [53] 最大子数组和
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0], preSum = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            if (preSum < 0) {
+                preSum = nums[i];
+            } else {
+                preSum = preSum + nums[i];
+            }
+            if (preSum > maxSum) {
+                maxSum = preSum;
+            }
+        }
+        return maxSum;
+    }
+}
+// @lc code=end
+
