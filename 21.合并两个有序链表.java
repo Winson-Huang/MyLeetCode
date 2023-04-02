22ca4b572c8aabe5358ca41c43d0000892cdf87b
WinsonHuang <hwangwc@qq.com>
2023-04-02 14:49:44 +0800

    21 recursion

--- 21.合并两个有序链表.java
+++ 21.合并两个有序链表.java
@@ -17,20 +17,15 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode prehead = new ListNode(-1, null);
-        ListNode prev = prehead;
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                prev.next = list1;
-                list1 = list1.next;
-            } else {
-                prev.next = list2;
-                list2 = list2.next;
-            } 
-            prev = prev.next;
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
-        prev.next = (list1 == null ? list2 : list1);
-        return prehead.next;
     }
 }
 // @lc code=end
