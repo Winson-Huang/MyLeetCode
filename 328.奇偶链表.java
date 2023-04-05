65abccee56c73bb25847b0e5861cfb1b1b2809ca
WinsonHuang <hwangwc@qq.com>
2023-04-05 11:48:08 +0800

    328

---
+++ 328.奇偶链表.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=java
+ *
+ * [328] 奇偶链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null) {
+            return head;
+        }
+        // need to store evenHead, after the while, head.next will change and 
+        // may not be head of even List
+        ListNode odd = head, even = head.next, evenHead = even;
+        while(odd.next != null && odd.next.next != null) {
+            odd.next = odd.next.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
+// @lc code=end
+
