8dd2a4cf52ebf4efd0278a0334f5c55ad27b9814
Winson <hwangwc@qq.com>
2023-04-09 00:11:41 +0800

    112 recursion easy

---
+++ 112.路径总和.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=java
+ *
+ * [112] 路径总和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean ans = false;
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        traversal(root, targetSum);
+        return ans;
+    }
+    public void traversal(TreeNode node, int residualSum) {
+        if (ans == true) return;
+        if (
+            node.left == null && node.right == null && 
+            residualSum == node.val
+        ) {
+            ans = true;
+            return;
+        }
+        if (node.left != null) {
+            traversal(node.left, residualSum - node.val);
+        }
+        if (node.right != null) {
+            traversal(node.right, residualSum - node.val);
+        }
+        return;
+    }
+}
+// @lc code=end
+
