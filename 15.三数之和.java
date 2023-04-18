1313e98fcae63567eff448781d7aa2a55aa8b027
Winson <hwangwc@qq.com>
2023-04-19 00:03:57 +0800

    15. two pointer

--- 15.三数之和.java
+++ 15.三数之和.java
@@ -14,33 +14,32 @@ import java.util.Map;
 // @lc code=start
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Map<Integer, Integer> numTable = new HashMap<>();
+        List<List<Integer>> ans = new LinkedList<>();
 
-        for (int i : nums) {
-            numTable.put(i, numTable.getOrDefault(i, 0) + 1);
-        }
-        Integer[] keys = numTable.keySet().toArray(new Integer[0]);
-        Arrays.sort(keys);
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1, right = nums.length - 1;
+            if (nums[i] > 0) return ans;
 
-        List<List<Integer>> ans = new LinkedList<List<Integer>>();
-        for (int i = 0; i < keys.length; i++) {
-            for (int j = i; j < keys.length; j++) {
-                numTable.put(keys[i], numTable.get(keys[i]) - 1);
-                numTable.put(keys[j], numTable.get(keys[j]) - 1);
-                Integer target = 0 - keys[i] - keys[j];
-                if (
-                    target >= keys[j] &&
-                    numTable.get(keys[i]) >= 0 &&
-                    numTable.get(keys[j]) >= 0 &&
-                    numTable.getOrDefault(target, 0) > 0
-                ) {
-                    ans.add(Arrays.asList(keys[i], keys[j], target));
+            if (i != 0 && nums[i-1] == nums[i]) {
+                continue;
+            }
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[right-1] == nums[right]) right--;
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+
+                    left++;
+                    right--;
                 }
-                numTable.put(keys[i], numTable.get(keys[i]) + 1);
-                numTable.put(keys[j], numTable.get(keys[j]) + 1);
             }
         }
-
         return ans;
     }
 }
