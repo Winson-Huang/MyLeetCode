8e5fb837316ed5d997ac6e87a45205b8006d3be7
Winson <hwangwc@qq.com>
2023-04-25 23:32:18 +0800

    94 recursion

---
+++ 94.二叉树的中序遍历.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=94 lang=java
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        traversal(root, ans);
+        return ans;
+    }
+    void traversal(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        traversal(node.left, list);
+        list.add(node.val);
+        traversal(node.right, list);
+    }
+}
+// @lc code=end
+
