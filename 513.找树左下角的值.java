a5ca177302a891eda47375f9ab9cbb91bfa9c720
WinsonHuang <hwangwc@qq.com>
2023-05-04 11:38:54 +0800

    513 iteration bfs

---
+++ 513.找树左下角的值.java
@@ -0,0 +1,47 @@
+import java.util.Queue;
+
+/*
+ * @lc app=leetcode.cn id=513 lang=java
+ *
+ * [513] 找树左下角的值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> que = new ArrayDeque<>();
+        que.offer(root);
+        int answer = root.val;
+        while (!que.isEmpty()) {
+            int length = que.size();
+            answer = que.peek().val;
+            for (int i = 0; i < length; i++) {
+                TreeNode node = que.poll();
+                if (node.left != null) {
+                    que.offer(node.left);
+                }
+                if (node.right != null) {
+                    que.offer(node.right);
+                }
+            }
+        }
+        return answer;
+    }
+}
+// @lc code=end
+
