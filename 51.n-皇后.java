d3a596ea14d05a6b41fcefaf183f6e886dff8c03
WinsonHuang <hwangwc@qq.com>
2023-06-26 22:47:28 +0800

    51 change check valid logic

--- 51.n-皇后.java
+++ 51.n-皇后.java
@@ -11,14 +11,18 @@ import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
-    boolean[][] board;
     List<List<String>> ans;
-    LinkedList<String> path;
+    LinkedList<Integer> path;
+    boolean[] column;
+    boolean[] slash;
+    boolean[] backslash;
 
     public List<List<String>> solveNQueens(int n) {
-        this.board = new boolean[n][n];
         ans = new ArrayList<>();
         path = new LinkedList<>();
+        this.column = new boolean[n];
+        this.slash = new boolean[2 * n - 1];
+        this.backslash = new boolean[2 * n - 1];
 
         backtracking();
 
@@ -26,48 +30,53 @@ class Solution {
     }
 
     void backtracking() {
-        if (path.size() == board.length) {
-            ans.add(new ArrayList<>(path));
+        if (path.size() == column.length) {
+            // generate String after finding a complete path, 
+            // do not generate String during path
+            ans.add(genStrRep(path));
             return;
         }
 
-        for (int i = 0; i < board.length; i++) {
+        for (int i = 0; i < column.length; i++) {
             if (!isValid(path.size(), i)) {
                 continue;
             }
 
-            board[path.size()][i] = true;
-            path.offerLast(genLine(i));
+            column[i] = true;
+            slash[path.size()+i] = true;
+            backslash[path.size() - i + column.length - 1] = true;
+
+            path.offerLast(i);
             backtracking();
             path.pollLast();
-            board[path.size()][i] = false;
+
+            column[i] = false;
+            slash[path.size()+i] = false;
+            backslash[path.size() - i + column.length - 1] = false;
         }
     }
 
     boolean isValid(int i, int j) {
         // no need to check row
-        // check column
-        for (int k = 0; k < i; k++) {
-            if (board[k][j]) return false;
-        }
-        // check lefttop
-        for (int k = i - 1, l = j - 1; k >=0 && l >= 0; k--, l--) {
-            if (board[k][l]) return false;
-        }
-        // check righttop
-        for (int k = i - 1, l = j + 1; k >=0  && l < board.length; k--, l++) {
-            if (board[k][l]) return false;
-        }
-        return true;
+        if (
+            !column[j] && !slash[i + j] && 
+            !backslash[i - j + column.length - 1]
+        ) return true;
+        return false;
     }
 
-    String genLine(int index) {
-        StringBuilder sb = new StringBuilder(board.length);
-        for (int i = 0; i < board.length; i++) {
+    List<String> genStrRep(List<Integer> path) {
+        StringBuilder sb = new StringBuilder(column.length);
+        for (int i = 0; i < column.length; i++) {
             sb.append('.');
         }
-        sb.setCharAt(index, 'Q');
-        return sb.toString();
+        ArrayList<String> pathStrs = new ArrayList<>(column.length);
+        for (Integer i : path) {
+            sb.setCharAt(i, 'Q');
+            pathStrs.add(sb.toString());
+            sb.setCharAt(i, '.');
+        }
+        return pathStrs;
     }
 
 }
