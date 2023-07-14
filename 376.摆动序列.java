eb8b1e0088cd6c931a702f044bc2a7214c163faa
WinsonHuang <hwangwc@qq.com>
2023-07-14 20:32:54 +0800

    376 greedy concise version

--- 376.摆动序列.java
+++ 376.摆动序列.java
@@ -7,31 +7,20 @@
 // @lc code=start
 class Solution {
     public int wiggleMaxLength(int[] nums) {
+        if (nums.length == 1) return 1;
+
         int count = 1;
-        int i = 1;
+        int prediff = 0;
+        int lastNum = nums[0];
 
-        while (i < nums.length && nums[i] == nums[0]) {
-            i++;
-        }
-        if (i >= nums.length) {
-            return count;
-        }
-
-        count++;
-        boolean needPosDelta = true;
-        if (nums[i] - nums[0] > 0) {
-            needPosDelta = false;
-        }
-
-        int lastNum = nums[i];
-        for (int j = i + 1; j < nums.length; j++) {
+        for (int j = 1; j < nums.length; j++) {
             if (
-                needPosDelta && nums[j] - lastNum > 0 ||
-                !needPosDelta && nums[j] - lastNum < 0
+                prediff <= 0 && nums[j] - lastNum > 0 ||
+                prediff >= 0 && nums[j] - lastNum < 0
             ) {
                 count++;
+                prediff = nums[j] - lastNum;
                 lastNum = nums[j];
-                needPosDelta = !needPosDelta;
             } else {
                 lastNum = nums[j];
             }
