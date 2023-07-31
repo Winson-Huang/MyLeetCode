efe8ada7838f91feca0ca4d176bc4804e9d1eea6
WinsonHuang <hwangwc@qq.com>
2023-07-31 22:45:17 +0800

    406  from short to tall

--- 406.根据身高重建队列.java
+++ 406.根据身高重建队列.java
@@ -12,20 +12,34 @@ import java.util.List;
 // @lc code=start
 class Solution {
     public int[][] reconstructQueue(int[][] people) {
+
         Arrays.sort(people, new Comparator<int[]>() {
             public int compare(int[] p1, int[] p2) {
-                return p1[0] == p2[0] ? p1[1] - p2[1] : p2[0] - p1[0];
+                return p1[0] == p2[0] ? p2[1] - p1[1] : p1[0] - p2[0];
             }
         });
 
         // faster than LinkedList
-        List<int[]> queue = new ArrayList<>();
+        // init in this way, element in allNulls is null (allNulls[0] == null is true)
+        int[][] allNulls = new int[people.length][];
+        // element in ArrayList is allowed to be null
+        List<int[]> queue = new ArrayList<>(Arrays.asList(allNulls));
+
         for (int[] oneperson: people) {
-            queue.add(oneperson[1], oneperson);
+            int index = 0;
+            int countNull = oneperson[1];
+            while (countNull >= 0) {
+                if (queue.get(index++) == null) {
+                    countNull--;
+                }
+            }
+
+            queue.set(--index, oneperson);
         }
 
         return queue.toArray(new int[people.length][2]);
     }
+
 }
 // @lc code=end
 
