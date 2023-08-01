ba6d254df05db39e976325459518305e5000f327
WinsonHuang <hwangwc@qq.com>
2023-08-01 22:35:31 +0800

    435 rewrite greedy

--- 435.无重叠区间.java
+++ 435.无重叠区间.java
@@ -10,25 +10,21 @@ import java.util.Comparator;
 // @lc code=start
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        int iLength = intervals.length;
-        if (iLength == 0)
-            return 0;
         Arrays.sort(intervals, new Comparator<int[]>() {
-            @Override
             public int compare(int[] o1, int[] o2) {
-                return o1[1] < o2[1] ? -1 : (o1[1] == o2[1] ? 0 : 1);
+                return o1[1] < o2[1] ? -1 : o1[1] == o2[1] ? 0 : 1;
             }
         });
-        int end = intervals[0][1];
+
         int count = 1;
-        for (int i = 1; i < iLength; i++) {
-            if (intervals[i][0] < end) {
-                continue;
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= end) {
+                count++;
+                end = intervals[i][1];
             }
-            end = intervals[i][1];
-            count++;
         }
-        return iLength - count;
+        return intervals.length - count;
     }
 }
 // @lc code=end
