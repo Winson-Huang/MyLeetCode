4fc86fb7912f8add5973212ad7a7f42ab5b99731
WinsonHuang <hwangwc@qq.com>
2023-08-27 01:20:07 +0800

    42 two pointers dp

--- 42.接雨水.java
+++ 42.接雨水.java
@@ -7,20 +7,24 @@
 // @lc code=start
 class Solution {
     public int trap(int[] height) {
-        // dynamic programming
-        int[] leftMax = new int[height.length];
-        int[] rightMax = new int[height.length];
-        leftMax[0] = height[0];
-        rightMax[height.length - 1] = height[height.length - 1];
-
-        for (int i = 1; i < height.length; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
-            rightMax[height.length - i - 1] = Math.max(rightMax[height.length - i], height[height.length - i - 1]);
-        }
+        // dynamic programming: two pointers
+        // two pointers method is actually a variant of dp
+        int leftMax = height[0];
+        int leftPointer = 0;
+        int rightMax = height[height.length - 1];
+        int rightPointer = height.length - 1;
 
         int sum = 0;
-        for (int i = 0; i < height.length; i++) {
-            sum += Math.min(leftMax[i], rightMax[i]) - height[i];
+        while (leftPointer < rightPointer) {
+            if (leftMax < rightMax) {
+                leftPointer++;
+                leftMax = Math.max(leftMax, height[leftPointer]);
+                sum += leftMax - height[leftPointer];
+            } else {
+                rightPointer--;
+                rightMax = Math.max(rightMax, height[rightPointer]);
+                sum += rightMax - height[rightPointer];
+            }
         }
 
         return sum;
