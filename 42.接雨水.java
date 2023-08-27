f5ecc32b301396b29a4229494d076a8477ec9fe5
WinsonHuang <hwangwc@qq.com>
2023-08-27 12:52:51 +0800

    42 monotonic stack

--- 42.接雨水.java
+++ 42.接雨水.java
@@ -5,26 +5,27 @@
  */
 
 // @lc code=start
+
+
 class Solution {
     public int trap(int[] height) {
-        // dynamic programming: two pointers
-        // two pointers method is actually a variant of dp
-        int leftMax = height[0];
-        int leftPointer = 0;
-        int rightMax = height[height.length - 1];
-        int rightPointer = height.length - 1;
+        // monotonic stack
+        // use an array to simulate stack
+        int[] stack = new int[height.length];
+        int pointer = -1;
 
         int sum = 0;
-        while (leftPointer < rightPointer) {
-            if (leftMax < rightMax) {
-                leftPointer++;
-                leftMax = Math.max(leftMax, height[leftPointer]);
-                sum += leftMax - height[leftPointer];
-            } else {
-                rightPointer--;
-                rightMax = Math.max(rightMax, height[rightPointer]);
-                sum += rightMax - height[rightPointer];
+        for (int i = 0; i < height.length; i++) {
+            while (pointer > 0 && height[stack[pointer]] <= height[i]) {
+                sum += (Math.min(height[stack[pointer - 1]], height[i])
+                        - height[stack[pointer]])
+                        * (i - 1 - stack[pointer - 1]);
+                pointer--;
             }
+            if (pointer == 0 && height[stack[pointer]] <= height[i]) {
+                pointer--;
+            }
+            stack[++pointer] = i;
         }
 
         return sum;
