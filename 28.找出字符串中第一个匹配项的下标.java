e2616b2ea7025f406c57f650db4cebc36ba7e354
WinsonHuang <hwangwc@qq.com>
2023-09-06 16:34:54 +0800

    28 kmp rewrite

--- 28.找出字符串中第一个匹配项的下标.java
+++ 28.找出字符串中第一个匹配项的下标.java
@@ -10,29 +10,37 @@ import java.util.Arrays;
 class Solution {
     public int strStr(String haystack, String needle) {
 
-        // fail[i] is j, then needle[0, j] == needle[i-j, i]
-        int lenNee = needle.length();
-        int[] fail = new int[lenNee - 1];
-
+        // fail[i] is j, means needle[0, j] equals needle[i-j, i]
+        // j >= -1 and j <= i - 1
+        int[] fail = new int[needle.length() - 1];
         Arrays.fill(fail, -1);
+
         for (int j = -1, i = 1; i < fail.length; i++) {
+            //in beginning of every iteration in this for-loop,
+            // j always stores fail[i - 1]
             while (j != -1 && needle.charAt(j + 1) != needle.charAt(i)) {
                 j = fail[j];
             }
+
             if (needle.charAt(j + 1) == needle.charAt(i)) {
                 j++;
             }
+
             fail[i] = j;
         }
 
+
         for (int j = -1, i = 0; i < haystack.length(); i++) {
-            while (j != -1 && haystack.charAt(i) != needle.charAt(j + 1)) {
+
+            while (j != -1 && needle.charAt(j + 1) != haystack.charAt(i)) {
                 j = fail[j];
             }
-            if (haystack.charAt(i) == needle.charAt(j + 1)) {
+
+            if (needle.charAt(j + 1) == haystack.charAt(i)) {
                 j++;
-            } 
-            if (j == lenNee - 1) {
+            }
+
+            if (j == needle.length() - 1) {
                 return i - j;
             }
         }
