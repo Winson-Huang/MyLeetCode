0f687e0f756a7d741f6b2c3e80cc7c598b53e06d
WinsonHuang <hwangwc@qq.com>
2023-09-12 19:33:34 +0800

    322 knapsack add comment

--- 322.零钱兑换.java
+++ 322.零钱兑换.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
 
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        // 该版本代码是通用完全背包思路（外层循环物品，内层循环容量）的解法，
+        // 并且将二维 dp 数组压缩为一维 dp 数组。与力扣官方题解思路不同。
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, 1, amount + 1, Integer.MAX_VALUE);
         for (int i = 0; i < coins.length; i++) {
