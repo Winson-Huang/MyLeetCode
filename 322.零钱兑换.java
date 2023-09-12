ae9fcdaf3384fc9db68e87f38ca8d775f4746a79
WinsonHuang <hwangwc@qq.com>
2023-09-12 19:40:21 +0800

    322 dp

--- 322.零钱兑换.java
+++ 322.零钱兑换.java
@@ -10,19 +10,18 @@ import java.util.Arrays;
 
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        // 该版本代码是通用完全背包思路（外层循环物品，内层循环容量）的解法，
-        // 并且将二维 dp 数组压缩为一维 dp 数组。与力扣官方题解思路不同。
+        // this is leetcode official solution's method
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, 1, amount + 1, Integer.MAX_VALUE);
-        for (int i = 0; i < coins.length; i++) {
-            for (int j = coins[i]; j < dp.length; j++) {
-                if (dp[j - coins[i]] != Integer.MAX_VALUE) {
-                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = amount + 1;
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
-        if (dp[amount] == Integer.MAX_VALUE) return -1;
-        return dp[amount];
+
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 }
 // @lc code=end
