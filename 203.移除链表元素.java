df2a4a2e5c10eb97a27a66b3fe1ce44e789c6882
WinsonHuang <hwangwc@qq.com>
2023-10-16 16:46:41 +0800

    203 use a dummy node

---
+++ 203.移除链表元素.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=java
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummyNode = new ListNode(0, head);
+        ListNode tmp = dummyNode;
+        while (tmp.next != null) {
+            if (tmp.next.val == val) {
+                tmp.next = tmp.next.next;
+            } else {
+                tmp = tmp.next;
+            }
+        }
+        return dummyNode.next;
+    }
+}
+// @lc code=end
+
