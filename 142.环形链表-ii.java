94ae221b85eb4561a628f3d3f79bf418c53003d1
WinsonHuang <hwangwc@qq.com>
2023-10-18 20:35:33 +0800

    142 two pointers

---
+++ 142.环形链表-ii.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=java
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) return null;
+        // 保证 slow 和 fast 的起始位置相同
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode probe = head;
+                while (probe != slow) {
+                    probe = probe.next;
+                    slow = slow.next;
+                }
+                return probe;
+            }
+        }
+
+        return null;
+
+    }
+}
+// @lc code=end
+
