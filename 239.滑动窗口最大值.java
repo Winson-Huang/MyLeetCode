3da1382e1562f21fdebd4cb850595edd1f9f1f37
WinsonHuang <hwangwc@qq.com>
2024-01-30 00:48:34 +0800

    239 monotonic queue

--- 239.滑动窗口最大值.java
+++ 239.滑动窗口最大值.java
@@ -1,5 +1,6 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Queue;
 
 /*
  * @lc app=leetcode.cn id=239 lang=java
@@ -9,46 +10,63 @@ import java.util.Deque;
 
 // @lc code=start
 class Solution {
-    class DiminishQueue {
-        Deque<Integer> deque;
-        public DiminishQueue(int k) {
-            this.deque = new ArrayDeque<>(k);
-        }
-
-        public void push(int value) {
-            while (!deque.isEmpty() && deque.peekLast() < value) {
-                deque.pollLast();
-            }
-            deque.offerLast(value);
-        }
-
-        public void pop(int value) {
-            if (deque.isEmpty()) return; // this case wont appear
-            if (deque.peekFirst() == value) {
-                deque.pollFirst();
-            }
-        }
-
-        public int frontValue() {
-            return deque.peekFirst();
-        }
-    }
     public int[] maxSlidingWindow(int[] nums, int k) {
-        DiminishQueue diminishQueue = new DiminishQueue(k);
-        int[] ans = new int[nums.length - k + 1];
-
+        return maxSlidingWindowMonotonicQueue(nums, k);
+    }
+    
+    
+    // from leetcode official
+    int[] maxSlidingWindowMonotonicQueue(int[] nums, int k) {
+        MonotonicQueue numsQueue = new MonotonicQueue(nums, k);
         for (int i = 0; i < k; i++) {
-            diminishQueue.push(nums[i]);
+            numsQueue.add(i);
         }
-        ans[0] = diminishQueue.frontValue();
-
-        for (int i = 1; i < ans.length; i++) {
-            diminishQueue.pop(nums[i - 1]);
-            diminishQueue.push(nums[k + i - 1]);
-            ans[i] = diminishQueue.frontValue();
+        int[] ans = new int[nums.length - k + 1];
+        ans[0] = numsQueue.getMax();
+        
+        int windowLeft = 0;
+        for (int windowRight = k; windowRight < nums.length; windowRight++, windowLeft++) {
+            numsQueue.add(windowRight);
+            numsQueue.remove(windowLeft);
+            ans[windowRight - k + 1] = numsQueue.getMax();
         }
+        
         return ans;
     }
+    
+    class MonotonicQueue {
+        // monotonic queue is not exactly a "queue", in one side, it can add or remove elements to keep monotonic,
+        // in the other side, it can only remove elements, so we use a Deque rather a Queue
+        Deque<Integer> monotonicQ;
+        int[] nums;
+        public MonotonicQueue(int[] nums, int k) {
+            this.monotonicQ = new ArrayDeque<>(k);
+            this.nums = nums;
+        }
+        
+        // monotonic queue stores indices
+        public void add(int index) {
+            while (!this.monotonicQ.isEmpty() && nums[this.monotonicQ.peekLast()] <= nums[index]) {
+                this.monotonicQ.pollLast();
+            }
+            this.monotonicQ.offerLast(index);
+        }
+        
+        public void remove(int index) {
+
+            // in this problem, this case won't happen
+            if (this.monotonicQ.isEmpty()) return;
+
+            if (this.monotonicQ.peekFirst() <= index) {
+                this.monotonicQ.pollFirst();
+            }
+        }
+        
+        public Integer getMax() {
+            return nums[this.monotonicQ.peekFirst()];
+        }
+    }
+    
 }
 // @lc code=end
 
