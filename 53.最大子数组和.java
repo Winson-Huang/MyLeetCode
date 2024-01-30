c48eb8d1032d7463e06db56274757efa95232caf
WinsonHuang <hwangwc@qq.com>
2024-01-30 23:14:25 +0800

    rewrite 53

--- 53.最大子数组和.java
+++ 53.最大子数组和.java
@@ -7,15 +7,63 @@
 // @lc code=start
 class Solution {
 
-    // dp is more interpretable than greedy
     public int maxSubArray(int[] nums) {
-        int preSum = nums[0];
-        int maxSum = preSum;
+        // return maxSubArrayDp(nums);
+        return maxSubArrayDivideConquer(nums);
+    }
+
+    // dp is more interpretable than greedy
+    // from leetcode official
+    int maxSubArrayDp(int[] nums) {
+        int maxSumEndsAtI = nums[0];
+        int maxSum = maxSumEndsAtI;
+        
         for (int i = 1; i < nums.length; i++) {
-            preSum = Math.max(preSum + nums[i], nums[i]);
-            maxSum = Math.max(preSum, maxSum);
+            maxSumEndsAtI = Math.max(maxSumEndsAtI + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, maxSumEndsAtI);
         }
+        
         return maxSum;
+    }
+    
+
+    // divide and conquer is more powerful (but no better in time and space)
+    int maxSubArrayDivideConquer(int[] nums) {
+        return getStatus(nums, 0, nums.length - 1).maxSum;
+    }
+    
+    class Status {
+        int lmSum;  // max sum of subseq start from left  
+        int rmSum;  // max sum of subseq end in right    
+        int maxSum; // max sum of subseq                 
+        int sum;    // sum of all elements               
+    }
+    
+    Status getStatus(int[] nums, int left, int right) {
+        Status status = new Status();
+        // in fact, this case won't happen
+        if (left > right) {
+            status.lmSum = status.rmSum = status.maxSum = status.sum = 0;
+            return status;
+        }
+        if (left == right) {
+            status.lmSum = status.rmSum = status.maxSum = status.sum = nums[left];
+            return status;
+        }
+        
+        // in this case, left + right wont overflow, 
+        // but it's better to write as left + (right - left) / 2
+        int mid = (left + right) >> 1;
+        
+        Status leftStatus = getStatus(nums, left, mid);
+        Status righStatus = getStatus(nums, mid + 1, right);
+        status.lmSum = Math.max(leftStatus.lmSum, leftStatus.sum + righStatus.lmSum);
+        status.rmSum = Math.max(righStatus.rmSum, leftStatus.rmSum + righStatus.sum);
+        status.maxSum = Math.max(leftStatus.rmSum + righStatus.lmSum,
+            Math.max(leftStatus.maxSum, righStatus.maxSum));
+        status.sum = leftStatus.sum + righStatus.sum;
+        
+        return status;
     }
 }
 // @lc code=end
