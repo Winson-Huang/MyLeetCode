3d399c903cb282020c448479e7960ef4b54bd908
WinsonHuang <hwangwc@qq.com>
2024-02-22 22:07:16 +0800

    141 rewrite

--- 141.环形链表.java
+++ 141.环形链表.java
@@ -16,15 +16,21 @@
 // }
 class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null) return false;
-        ListNode a = head, b = head.next;
-        while (a != b) {
-            // only need to test if b is null. if b != null, a will never be null.
-            if (b == null || b.next == null) return false;
-            a = a.next;
-            b = b.next.next;
+        // inspired by leetcode official, but modified to be more consistent with
+        // 142.
+
+        if (head == null) return false;
+
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
         }
-        return true;
+        
+        return false;
     }
 }
 // @lc code=end
