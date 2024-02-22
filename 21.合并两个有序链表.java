5875963f3080c7a0fcaef1baa1c093c224725ce8
WinsonHuang <hwangwc@qq.com>
2024-02-22 22:22:54 +0800

    21 rewrite

--- 21.合并两个有序链表.java
+++ 21.合并两个有序链表.java
@@ -17,16 +17,29 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
-        if (list1.val <= list2.val) {
-            list1.next = mergeTwoLists(list1.next, list2);
-            return list1;
-        } else {
-            list2.next = mergeTwoLists(list1, list2.next);
-            return list2;
-        }
+        return mergeTwoListsIteration(list1, list2);
     }
+
+    ListNode mergeTwoListsIteration(ListNode list1, ListNode list2) {
+        ListNode prehead = new ListNode(-1);
+        ListNode cur = prehead;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                cur.next = list1;
+                list1 = list1.next;
+            } else {
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        
+        cur.next = list1 == null ? list2 : list1;
+
+        return prehead.next;
+    }
+
 }
 // @lc code=end
 
